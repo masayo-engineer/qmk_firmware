623fcc1d70af9286fd4334ac9535c2191ddc3c36
masayo-engineer <89111460+masayo-engineer@users.noreply.github.com>
2024-06-18 09:30:05 +0900

    Update config.h
    <add>
    SPLIT_HAND_PIN C7
    <comment out>
    MASTER_LEFT

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -24,9 +24,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
+#define SPLIT_HAND_PIN C7
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
